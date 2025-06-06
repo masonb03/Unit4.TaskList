04b4dcbf9c2f51ebccd3de658e683b4885575e2b
Mason <bevinsm355@gmail.com>
2025-06-05 20:18:01 -0400

    updated

--- db/queries/tasks.js
+++ db/queries/tasks.js
@@ -1,12 +1,39 @@
-import db from "#db/client.js";
+import db from "../client.js";
 
-export async function createTask({ title, done, user_id}) {
-    const res = await db.query(
-        `INSERT INTO tasks (title, done, user_id)
-        VALUES ($1, $2, $3)
-        RETURNING *;
-        `, [title, done, user_id]
-    );
 
-    return res.rows[0]
+export async function createTask({ title, done, user_id }) {
+  const sql = `INSERT INTO tasks (title, done, user_id) VALUES ($1, $2, $3) RETURNING *`;
+  const { rows } = await db.query(sql, [title, done, user_id]);
+  return rows[0];
+}
+
+
+export async function getTasksByUser(user_id) {
+  const sql = `SELECT * FROM tasks WHERE user_id = $1 ORDER BY id`;
+  const { rows } = await db.query(sql, [user_id]);
+  return rows;
+}
+
+
+export async function updateTask({ id, title, done, user_id }) {
+  const sql = `
+    UPDATE tasks
+    SET title = $1, done = $2
+    WHERE id = $3 AND user_id = $4
+    RETURNING *`;
+  const { rows } = await db.query(sql, [title, done, id, user_id]);
+  return rows[0];
+}
+
+
+export async function deleteTask(id, user_id) {
+  const sql = `DELETE FROM tasks WHERE id = $1 AND user_id = $2 RETURNING *`;
+  const { rows } = await db.query(sql, [id, user_id]);
+  return rows[0];
+}
+
+export async function getTaskById(id) {
+  const sql = `SELECT * FROM tasks WHERE id = $1`;
+  const { rows } = await db.query(sql, [id]);
+  return rows[0];
 }
--- db/queries/users.js
+++ db/queries/users.js
@@ -1,12 +1,15 @@
-import db from "#db/client.js";
+import db from "../client.js";
 
-export async function createUser({ name, password }){
-    const res = await db.query(
-        `INSERT INTO users (name, password)
-         VALUES ($1, $2) 
-         RETURNING *;
-         `, [name, password]
-        );
 
-        return res.rows[0]
+export async function getUserByName(name) {
+  const sql = "SELECT * FROM users WHERE name = $1";
+  const { rows } = await db.query(sql, [name]);
+  return rows[0];
+}
+
+
+export async function createUser({ name, password }) {
+  const sql = `INSERT INTO users (name, password) VALUES ($1, $2) RETURNING id, name`;
+  const { rows } = await db.query(sql, [name, password]);
+  return rows[0];
 }
