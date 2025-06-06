cdbfd6217abb705c10b7391f7c7ea9fb72614d7b
Mason <bevinsm355@gmail.com>
2025-06-05 20:00:03 -0400

    updated

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ import express from "express";
 const app = express();
 export default app;
 
+app.use(express.json())
+
+app.use((req, res, next) =>{
+  console.log(req.method, req.originalUrl)
+  next()
+})
+
+
+app.get('/', (req, res) =>{
+    res.send('Welcome to the backend application!')
+})
+
+
+
 
 app.use((err, req, res, next) => {
   console.error(err);
